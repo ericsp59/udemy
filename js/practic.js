62426933320c1174f7063ccfe2bc3a7a798ec654
ericsp <ericsp@mail.ru>
2020-07-29 15:55:44 +0500

    lesson 2-24

--- js/practic.js
+++ js/practic.js
@@ -20,69 +20,72 @@ function c(x){
     console.log(x);
 }
 
-let numberOfFilms;
-
-function start() {
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt(`Сколько фильмов вы уже посмотрели?`);
-    }
-}
-
-start();
-
 let personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: [],
     actors: {},
     genres: [],
-    privat: false
-};
-
-
-
-function rememberMyFilms() {
-    for(let i = 0; i < 2; i++){
-        const   a = prompt(`Один из последних просмотренных фильмов?`, ``),
-                b = prompt(`На сколько оцените его?`, ``);
-    
-        if(a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            c(`done`);
-        } else {
-            c(`error`);
-            i--;
+    privat: false,
+    start: function() {
+        while (this.count == '' || this.count == null || isNaN(this.count)) {
+            this.count = +prompt(`Сколько фильмов вы уже посмотрели?`);
         }
+    },    
+    rememberMyFilms: function() {
+        for(let i = 0; i < 2; i++){
+            const   a = prompt(`Один из последних просмотренных фильмов?`, ``),
+                    b = prompt(`На сколько оцените его?`, ``);
+        
+            if(a != null && b != null && a != '' && b != '' && a.length < 50) {
+                this.movies[a] = b;
+                c(`done`);
+            } else {
+                c(`error`);
+                i--;
+            }
+        }        
+    },
+    detectPersonalLevel: function(){
+        if (this.count < 10) {
+            c(`Просмотрено довольно мало фильмов`);
+        } else if (10 <= this.count && this.count < 30 ){
+            c(`Вы классический зритель`);
+        } else if ( this.count > 30 ){
+            c(`Вы киноман`);
+        } else {
+            c(`Произошла ошибка`);
+        }        
+    },
+    showMyDB: function(hidden){
+        if (!hidden) {
+            c(personalMovieDB);
+        }        
+    },
+    writeYourGenres: function(){
+        for (let i = 0; i < 1; i++) {
+            // this.genres[i] = prompt(`Ваш любимый жанр ${i+1}?`,'');
+            // if ( this.genres[i]== null || this.genres[i] =='' ){
+            //     i--;
+            // }
+            let genresLoc = prompt(`Введите любимые жанры через запятую `,'').toLowerCase();
+            if ( genresLoc == null || genresLoc =='' ){
+                i--;
+            }
+            this.genres = genresLoc.split(',');
+        }
+        this.genres.forEach((element, i) => {
+            c(`Любимый жанр ${i+1} - ${element}`);
+        });
+    },
+    toggleVisibleMyDB: function(){
+        this.privat = !this.privat;
     }
-}
+};
+personalMovieDB.start();
 
-rememberMyFilms();
-
-function detectPersonalLevel() {  
-    if (personalMovieDB.count < 10) {
-        c(`Просмотрено довольно мало фильмов`);
-    } else if (10 <= personalMovieDB.count && personalMovieDB.count < 30 ){
-        c(`Вы классический зритель`);
-    } else if ( personalMovieDB.count > 30 ){
-        c(`Вы киноман`);
-    } else {
-        c(`Произошла ошибка`);
-    }
-}
-
-detectPersonalLevel();
-
-function showMyDB() {
-    if (personalMovieDB.privat === false) {
-        c(personalMovieDB);
-    }
-}
-
-function writeYourGenres() {
-    for (let i = 0; i < 3; i++) {
-        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр ${i+1}?`,'');
-    }
-}
-
-writeYourGenres();
-
-showMyDB();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.writeYourGenres();
+personalMovieDB.toggleVisibleMyDB();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.toggleVisibleMyDB();
+personalMovieDB.showMyDB(personalMovieDB.privat);
--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,32 @@ function c(x){
     console.log(x);
 }
 
-
 //////////////////////// 2-23////////////////////////
+
+let str =  'some';
+// let strobj = new String(str);
+//c(typeof(str));
+// c(typeof(strobj));
+
+//c([1, 2, 3]);
+
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function(){
+        c('Hello!');
+    }
+};
+
+// const John = {
+//     health: 100
+// };
+
+// //John.__proto__ = soldier;
+// Object.setPrototypeOf(John, soldier);
+
+
+const John = Object.create(soldier);
+
+John.sayHello();
